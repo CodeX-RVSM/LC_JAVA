71779cf30779697673fe8ba32d734128e06f980b
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-01-29 17:33:02 +0530

    Create switch_case.java

---
+++ switch_case.java
@@ -0,0 +1,12 @@
+class switch_case {
+    public static void main(String[] args){
+        int a=5;
+        int b=5;
+        if(a==b){
+            System.out.println("Both are equal");
+        }
+        else{
+            System.out.println("Not equal");
+        }
+    }
+}
