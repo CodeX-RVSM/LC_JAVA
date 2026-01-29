d393871cc2e00aba97eff49da4df7e0de26a00c6
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-01-29 17:32:04 +0530

    Create less_than10.java

---
+++ less_than10.java
@@ -0,0 +1,11 @@
+class less_than10 {
+    public static void main(String[] args){
+        int a=7;
+        if(a<10){
+            System.out.println("7 is less than 10");
+        }
+        else{
+            System.out.println("7 is greater than 10");
+        }
+    }
+}
