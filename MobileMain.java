5c5986f7011d7499d2d02632f8b2b60ca0e863f7
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-12 22:37:32 +0530

    Create MobileMain.java

---
+++ MobileMain.java
@@ -0,0 +1,12 @@
+class MobileMain{
+
+    public static void main(String[] args) {
+        Mobile mobile1=new Mobile("Vivo",6.6f,128,40000,"Blue");
+        mobile1.getInfo();
+        Mobile mobile2=new Mobile("iphone",6.2f,256,60000,"Black");
+        mobile2.getInfo();
+        Mobile mobile3=new Mobile("Samsung",6.7f,128,50000,"white");
+        mobile3.getInfo();
+    }
+    
+}
