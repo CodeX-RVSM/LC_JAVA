c9d2a018069624077b607ae58171b01c0c4338a6
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-12 22:46:10 +0530

    Create hemet_main.java

---
+++ hemet_main.java
@@ -0,0 +1,10 @@
+public class helmet_main {
+    public static void main(String[] args) {
+        helmet helmet1= new helmet("XOR","XL","Full face",3500,"Black");
+        helmet1.getInfo();
+        helmet helmet2= new helmet("Vega","M","Full face",2500,"Black");
+        helmet1.getInfo();
+        helmet helmet3= new helmet("STUDDS","L","half face",4000,"Black");
+        helmet1.getInfo();
+    }
+}
