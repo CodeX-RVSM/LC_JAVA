02d99aa54f931247d7795d936587e760edb0bf14
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-17 16:35:17 +0530

    Clean up whitespace in Book.java
    Removed unnecessary blank lines in the Book class.

--- Book.java
+++ Book.java
@@ -1,36 +1,28 @@
 package com.linkcode.Home;
 
+
 public class Book {
 	String BookName;
 	String author;
 	int noOfPages;
 	double price;
-	
-	
 
+	
 	public Book(String bookName, String author, int noOfPages, double price) {
 		BookName = bookName;
 		this.author = author;
 		this.noOfPages = noOfPages;
 		this.price = price;
 	}
-
-
-
 	public void read() {
 		System.out.println("reading");
 		
 	}
-
-
-
 	public void displayInfo() {
 		System.out.println("Book Name :"+BookName);
 		System.out.println("Author :"+author);
 		System.out.println("Price :"+price);
 		System.out.println("No  OF Pages :"+noOfPages);
-		System.out.println("----------");
-		
+		System.out.println("----------");	
 	}
-
 }
