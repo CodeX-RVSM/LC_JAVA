317168da338d15252f211821976ce4ef25775bd0
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-24 22:41:06 +0530

    Create Anaimal.java

---
+++ inheritance/Anaimal.java
@@ -0,0 +1,17 @@
+package com.linkcode.inheritance;
+
+public class Animal {
+
+	    String color;
+	    int age;
+
+	    public void eat() {
+	        System.out.println("Animal is eating...");
+	    }
+
+	    public void sleep() {
+	        System.out.println("Animal is sleeping...");
+	    }
+	
+
+}
